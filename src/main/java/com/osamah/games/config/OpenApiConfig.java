1244757422044ca8a97472a443adcfda99e9bc4b
Osamah Mulhem <osamayousefosama@gmail.com>
2026-05-21 10:44:04 +0400

    _

--- src/main/java/com/osamah/games/config/OpenApiConfig.java
+++ src/main/java/com/osamah/games/config/OpenApiConfig.java
@@ -6,8 +6,11 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -16,9 +19,19 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
 
-        return new OpenAPI().info(new Info().title("Games API")
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("Local Development Environment");
+
+        Server productionServer = new Server();
+        productionServer.setUrl("https://games-api-production-d0f0.up.railway.app");
+        productionServer.setDescription("Production Environment on Railway");
+
+        return new OpenAPI()
+                .servers(List.of(productionServer, localServer))
+                .info(new Info().title("Games API")
                         .version("1.0")
-                        .description("API documentation for the " + "Video Game Library " + "Management" + " System")
+                        .description("API documentation for the Video Game Library Management System")
                         .contact(new Contact().name("Osamah")))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components().addSecuritySchemes(securitySchemeName,
